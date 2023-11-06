6fec86e8bfaacf302d234f9297f4d7a0f472a71b
lexandr10 <133785276+lexandr10@users.noreply.github.com>
2023-11-06 21:12:53 +0000

    Deploying to gh-pages from @ lexandr10/goit-advancedjs-hw-01@bb0641085a2699cb3cde0da8d43d26687e6e2a2d 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{P as o,l as r}from"./assets/vendor-d78ab9e0.js";const a="videoplayer-current-time",l=document.querySelector("#vimeo-player"),e=new o(l),n=function(t){localStorage.setItem(a,t.seconds)};e.on("timeupdate",r(n,1e3));e.setCurrentTime(JSON.parse(localStorage.getItem(VCT_KEY))||0);
+import"./assets/common-75a3e929.js";import{P as r,l as a}from"./assets/vendor-d78ab9e0.js";const e="videoplayer-current-time",l=document.querySelector("#vimeo-player"),t=new r(l),n=function(o){localStorage.setItem(e,o.seconds)};t.on("timeupdate",a(n,1e3));t.setCurrentTime(localStorage.getItem(e)||0);
 //# sourceMappingURL=commonHelpers2.js.map
